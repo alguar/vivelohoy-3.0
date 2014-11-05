fe5f2c0934be08cfd8f97bf12dbd87fb7b422112
Lucio Villa <contact@luciovilla.com>
2014-11-05 13:17:30 -0600

    Change subpackage and text domain in content.php to gabo and removed Spanish translations.

--- wp-content/themes/gabo/content.php
+++ wp-content/themes/gabo/content.php
@@ -5,14 +5,14 @@
  * Used for both single and index/archive/search.
  *
  * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
+ * @subpackage Gabo
+ * @since Gabo 1.0
  */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header" style="text-align:center">
-		
+
 
 		<?php if ( is_single() ) : ?>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
@@ -23,11 +23,10 @@
 		<?php endif; // is_single() ?>
 
 		<div class="author-cat" style="margin:0">
-			
-				<!-- TODO: Add translation query here instead of hard-coded text -->
-				Por <?php the_author_posts_link(); ?> en <?php $category = get_the_category(); if($category[0]){ echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?> <?php the_time('m/j/y g:ia') ?>
-		
-		<?php edit_post_link( __( 'Edit', 'twentythirteen-child' ), '<span class="edit-link">', '</span>' ); ?>
+
+				By <?php the_author_posts_link(); ?> in <?php $category = get_the_category(); if($category[0]){ echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?> <?php the_time('m/j/y g:ia') ?>
+
+		<?php edit_post_link( __( 'Edit', 'gabo' ), '<span class="edit-link">', '</span>' ); ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
@@ -37,9 +36,9 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen-child' ) ); ?>
-	
-	 <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen-child' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?> 
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'gabo' ) ); ?>
+
+	 <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'gabo' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?> 
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
