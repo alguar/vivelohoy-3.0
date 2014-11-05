052bbc7cfc9417593b5110b9f2305b54699de478
Lucio Villa <contact@luciovilla.com>
2014-11-05 15:06:14 -0600

    Change subpackage in taxonomy-post_format.php to gabo.

--- wp-content/themes/gabo/taxonomy-post_format.php
+++ wp-content/themes/gabo/taxonomy-post_format.php
@@ -9,31 +9,31 @@
  * @link http://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
+ * @subpackage Gabo
+ * @since Gabo 1.0
  */
 
 get_header('home'); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main" style="max-width:960px">
-	
-<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>
-			<div id="top-leaderboard" class="adslot leaderboard" data-width="728" data-height="90" data-pos="1"></div>
-<?php endif; // End if ( $ADS_ENABLED ) ?>
-					
-	<?php include('home-loop.php') ?>
 
-	<?php
-		if (function_exists(custom_pagination)) {
-			custom_pagination($custom_query->max_num_pages,"",$paged);
-		}
-	?>
-				
-<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>	     
-			<div id="bottom-leaderboard" class="adslot leaderboard" data-width="728" data-height="90" data-pos="4"></div> 
-<?php endif; // End if ( $ADS_ENABLED ) ?>
-	
+			<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>
+						<div id="top-leaderboard" class="adslot leaderboard" data-width="728" data-height="90" data-pos="1"></div>
+			<?php endif; // End if ( $ADS_ENABLED ) ?>
+
+				<?php include('home-loop.php') ?>
+
+				<?php
+					if (function_exists(custom_pagination)) {
+						custom_pagination($custom_query->max_num_pages,"",$paged);
+					}
+				?>
+
+			<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>
+						<div id="bottom-leaderboard" class="adslot leaderboard" data-width="728" data-height="90" data-pos="4"></div>
+			<?php endif; // End if ( $ADS_ENABLED ) ?>
+
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
