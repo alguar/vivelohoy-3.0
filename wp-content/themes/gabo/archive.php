68827b8bc750c23971a630d20de6f6f69d2e782b
Nick Bennett <nbennett@vivelohoy.com>
2014-11-05 11:45:40 -0600

    Update function names in templates to the renamed gabo functions defined in functions.php.

--- wp-content/themes/gabo/archive.php
+++ wp-content/themes/gabo/archive.php
@@ -42,7 +42,7 @@ get_header(); ?>
 				<?php get_template_part( 'content', get_post_format() ); ?>
 			<?php endwhile; ?>
 
-			<?php twentythirteen_paging_nav(); ?>
+			<?php gabo_paging_nav(); ?>
 
 		<?php else : ?>
 			<?php get_template_part( 'content', 'none' ); ?>
