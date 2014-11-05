55371a4276ccf4a63e026977620e59e007b117bf
Lucio Villa <contact@luciovilla.com>
2014-11-05 12:19:01 -0600

    Updated 404.php

--- wp-content/themes/gabo/404.php
+++ wp-content/themes/gabo/404.php
@@ -3,16 +3,16 @@
  * The template for displaying 404 pages (Not Found)
  *
  * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
+ * @subpackage Gabo
+ * @since Gabo 1.0
  */
 
 get_header(); ?>
 </div>
 
 <div style="margin: 70px auto; max-width:860px">
-	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/405.jpg" alt="Error Cuatro-Cero-Cuatro..." width="100%">
-					
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/405.jpg" width="100%">
+
 				<div style="text-align:center">
 					<h2 class="error404-search"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'gabo' ); ?></h2>
 					<p style="margin: 0 0 5px 0; padding:0 10px"><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'gabo' ); ?></p>
@@ -22,8 +22,8 @@ get_header(); ?>
 							<input type="search" class="search-field" placeholder="Buscar…" value="" name="s" title="Buscar:">
 						</label>
 					</form>
-					
+
 				</div>
-</div>			
+</div>
 
 <?php get_footer(); ?>
