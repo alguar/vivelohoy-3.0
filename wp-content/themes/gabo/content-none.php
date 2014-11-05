8d00dbb53919da543de5784bcfb66ea2fbb747aa
Lucio Villa <contact@luciovilla.com>
2014-11-05 12:45:18 -0600

    Change subpackage and text domain in content-none.php to gabo.

--- wp-content/themes/gabo/content-none.php
+++ wp-content/themes/gabo/content-none.php
@@ -3,28 +3,28 @@
  * The template for displaying a "No posts found" message
  *
  * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
+ * @subpackage Gabo
+ * @since Gabo 1.0
  */
 ?>
 
 <header class="page-header">
-	<h1><?php _e( 'Nothing Found', 'twentythirteen-child' ); ?></h1>
+	<h1><?php _e( 'Nothing Found', 'gabo' ); ?></h1>
 </header>
 
 <div class="page-content">
 	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-	<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentythirteen-child' ), admin_url( 'post-new.php' ) ); ?></p>
+	<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'gabo' ), admin_url( 'post-new.php' ) ); ?></p>
 
 	<?php elseif ( is_search() ) : ?>
 
-	<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with different keywords.', 'twentythirteen-child' ); ?></p>
+	<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with different keywords.', 'gabo' ); ?></p>
 	<?php get_search_form(); ?>
 
 	<?php else : ?>
 
-	<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentythirteen-child' ); ?></p>
+	<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'gabo' ); ?></p>
 	<?php get_search_form(); ?>
 
 	<?php endif; ?>
