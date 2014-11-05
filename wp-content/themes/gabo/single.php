24527c03a4f1e5da44e3a4e3c0ca57f53ad99ed7
Lucio Villa <contact@luciovilla.com>
2014-11-05 15:53:10 -0600

    Change subpackage in single.php to Gabo.

--- wp-content/themes/gabo/single.php
+++ wp-content/themes/gabo/single.php
@@ -3,21 +3,21 @@
  * The template for displaying all single posts
  *
  * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
+ * @subpackage Gabo
+ * @since Gabo 1.0
  */
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-	
+
 			<?php /* The loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>	
+			<?php get_template_part( 'content', get_post_format() ); ?>
 			<?php endwhile; ?>
 			<?php gabo_post_nav(); ?>
-			
+
 			<script>
 			var is_gallery = <?php if ( 'gallery' === get_post_format() ) : ?>true<?php else: ?>false<?php endif; ?>;
 			</script>
