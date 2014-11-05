e0bde7a294673a56a286da1c18cb4e05892c4901
Lucio Villa <contact@luciovilla.com>
2014-11-05 15:01:02 -0600

    Change subpackage and text domain in image.php to gabo and removed exif table.

--- wp-content/themes/gabo/image.php
+++ wp-content/themes/gabo/image.php
@@ -5,8 +5,8 @@
  * @link http://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
+ * @subpackage Gabo
+ * @since Gabo 1.0
  */
 
 get_header(); ?>
@@ -16,8 +16,8 @@ get_header(); ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class( 'image-attachment' ); ?>>
 				<div class="entry-content" style="padding:20px 0">
 					<nav id="image-navigation" class="navigation image-navigation" role="navigation">
-                        <span class="nav-previous" style="left: -117px"><?php previous_image_link( false, __( '<span class="meta-nav">&larr;</span> Previous', 'twentythirteen-child' ) ); ?></span>
-                        <span class="nav-next" style="right: -84px"><?php next_image_link( false, __( 'Next <span class="meta-nav">&rarr;</span>', 'twentythirteen-child' ) ); ?></span>
+                        <span class="nav-previous" style="left: -117px"><?php previous_image_link( false, __( '<span class="meta-nav">&larr;</span> Previous', 'gabo' ) ); ?></span>
+                        <span class="nav-next" style="right: -84px"><?php next_image_link( false, __( 'Next <span class="meta-nav">&rarr;</span>', 'gabo' ) ); ?></span>
                     </nav><!-- #image-navigation -->
 
 					<div class="entry-attachment" style="max-width: 960px; text-align: center">
@@ -30,99 +30,14 @@ get_header(); ?>
 							</div>
 
 							<?php endif; ?>
-                            <?php $imgmeta = wp_get_attachment_metadata( $id );?>  
-                            <!-- 
-                                EXIF data removed until perfected
-                            
-                            <?php if (0 != $imgmeta['image_meta']['iso']) : ?>
-							<div class="entry-exif">
-            					<div class="exif-table" >
-                                    <table >
-                                        <tr>
-                                            <h1>EXIF</h1>
-                                        </tr>
-                                        <tr>
-                                            <td >
-                                                Fuente
-                                            </td>
-                                            <td>
-                                                <?php echo $imgmeta['image_meta']['copyright'];?>
-                                            </td>
-                                          
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                Fotógrafo
-                                            </td>
-                                            <td>
-                                               <?php echo $imgmeta['image_meta']['credit'];?>
-                                            </td>
-                                           
-                                        </tr>
-                                        <tr>
-                                            <td >
-                                                 Camera
-                                            </td>
-                                            <td>
-                                                 <?php echo $imgmeta['image_meta']['camera'];?>
-                                            </td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td >
-                                                ISO
-                                            </td>
-                                            <td>
-                                                 <?php echo $imgmeta['image_meta']['iso'];?>
-                                            </td>
-                                           
-                                        </tr>
-                                         <tr>
-                                            <td >
-                                                Distancia focal
-                                            </td>
-                                            <td>
-                                                <?php echo $imgmeta['image_meta']['focal_length'].mm;?>
-                                            </td>
-                                           
-                                        </tr>
-                                        <tr>
-                                            <td >
-                                                Abertura
-                                            </td>
-                                            <td>
-                                                <?php  echo "f/" . $imgmeta['image_meta']['aperture']?>
-                                            </td>
-                                           
-                                        </tr>
-                                        <tr>
-                                            <td >
-                                                Tiempo de exposición
-                                            </td>
-                                            <td>
-                                                <?php echo $imgmeta['image_meta']['shutter_speed'] ?>
-                                            </td>
-                                           
-                                        </tr>
-                                    </table>
-                                </div>
-                                
-                                exif-table -->
+                            <?php $imgmeta = wp_get_attachment_metadata( $id );?>
 
-                            </div><!-- .entry-exif -->
-                            <?php endif; ?>
-            
 						</div><!-- .attachment -->
 					</div><!-- .entry-attachment -->
 
-					
-					
 				</div><!-- .entry-content -->
 			</article><!-- #post -->
-			
 			<hr>
-			<?php //comments_template(); ?>
-			
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
