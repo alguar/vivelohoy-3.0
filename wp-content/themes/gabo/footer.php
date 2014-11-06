50910a08782c485b9c6ada038ff0f52ebfee2f0f
Nick Bennett <nbennett@vivelohoy.com>
2014-11-06 11:26:51 -0600

    Remove Hoy-specific content from footer.php.

--- wp-content/themes/gabo/footer.php
+++ wp-content/themes/gabo/footer.php
@@ -4,22 +4,15 @@
  *
  * Contains footer content and the closing of the #main and #page div elements.
  *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
  */
 ?>
 
 		</div><!-- #main -->
 		<footer class="entry-meta">
 				<div id="footer-content"> 
-						© 2014 Hoy      
-			            <a href="/about-vivelohoy/"> Acerca de nosotros</a>
-			            <a href="/advertise"> Advertise</a>
-			            <a href="/terminos-de-servicio/"> Términos de servicio</a>
-			 			<a href="/politica-de-confidencialidad"> Política de privacidad</a>
+						© 2014
 				</div>
-			</footer><!-- #colophon -->
+			</footer>
 	</div><!-- #page -->
 	<?php wp_footer(); ?>
 </body>
